a7fd45dd35c657b8a21248b137fdc59c716eb6b5
CMOISDEAD <camilodavina@gmail.com>
2023-08-19 21:00:27 -0500

    feat: push, popByID methods implemented
    README.md created

--- src/lib/main.ts
+++ src/lib/main.ts
@@ -8,7 +8,7 @@ const save = (key: string, value: any) => {
 // return an item from localStorage
 const get = (key: string) => {
   const value = window.localStorage.getItem(key);
-  if (!value) throw new Error("no item with that name");
+  if (!value) throw new Error(`no item ${key} found on localStorage`);
   return JSON.parse(value);
 };
 
@@ -23,9 +23,31 @@ const clear = () => {
   window.localStorage.clear();
 };
 
+const isArray = (list: any) => {
+  if (!Array.isArray(list)) throw new Error(`is not an array`);
+};
+
+// push a value to an item on localStorage
+const push = (key: string, value: any) => {
+  const list = get(key);
+  isArray(list);
+  list.push(value);
+  save(key, value);
+};
+
+// remove an specific value by index from an item in localStorage
+const popByIndex = (key: string, index: number) => {
+  const list = get(key);
+  isArray(list);
+  list.filter((_: any, i: number) => i !== index);
+  save(key, list);
+};
+
 export default {
   get,
   save,
   remove,
+  push,
+  popByIndex,
   clear,
 };
